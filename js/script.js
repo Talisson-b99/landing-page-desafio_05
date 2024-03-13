1eb2e4ffff98c3bd7bff562ac849a94a757272f0
talisson <talissonb422@gmail.com>
2024-03-13 02:49:27 -0300

    commit initial

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// accordion
+document.querySelectorAll('.button-accordion').forEach((button) => {
+  button.addEventListener('click', () => {
+    const accordionContent = button.nextElementSibling
+    accordionContent.classList.toggle('active')
+
+    if(accordionContent.classList.contains('active')) {
+      accordionContent.style.height = accordionContent.scrollHeight + 'px'
+    } else {
+      accordionContent.style.height = 0
+    }
+  })
+})
+
+// menu mobile
+const btnOpenMenu = document.querySelector('.btn-menu')
+const btnCloseMenu = document.querySelector('.btn-close')
+const menu = document.querySelector('.menu-mobile')
+
+btnOpenMenu.addEventListener('click', () => {
+  menu.classList.add('menu-active')
+})
+
+btnCloseMenu.addEventListener('click', () => {
+  if(menu.classList.contains('menu-active')) {
+    menu.classList.remove('menu-active')
+  }
+})
